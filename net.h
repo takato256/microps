1014b72571b580203fb43a7d4c49ad8a49f0f16d
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-08-24 09:14:27 +0000

    complete step2

--- net.h
+++ net.h
@@ -23,28 +23,28 @@
 #define NET_DEVICE_IS_UP(x) ((x)->flags & NET_DEVICE_FLAG_UP)
 #define NET_DEVICE_STATE(x) (NET_DEVICE_IS_UP(x) ? "up" : "down")
 
-struct net_device{
-	struct net_device *next;
-	unsigned int index;
-	char name[IFNAMSIZ];
-	uint16_t type;
-	uint16_t mtu;
-	uint16_t flags;
-	uint16_t hlen; /* header length */
-	uint16_t alen; /* address length */
-	uint8_t addr[NET_DEVICE_ADDR_LEN];
-	union{
-		uint8_t peer[NET_DEVICE_ADDR_LEN];
-		uint8_t broadcast[NET_DEVICE_ADDR_LEN];
-	};
-	struct net_device_ops * ops;
-	void *priv;
+struct net_device {
+    struct net_device *next;
+    unsigned int index;
+    char name[IFNAMSIZ];
+    uint16_t type;
+    uint16_t mtu;
+    uint16_t flags;
+    uint16_t hlen; /* header length */
+    uint16_t alen; /* address length */
+    uint8_t addr[NET_DEVICE_ADDR_LEN];
+    union {
+        uint8_t peer[NET_DEVICE_ADDR_LEN];
+        uint8_t broadcast[NET_DEVICE_ADDR_LEN];
+    };
+    struct net_device_ops *ops;
+    void *priv;
 };
 
-struct net_device_ops{
-	int (*open)(struct net_device *dev);
-	int (*close)(struct net_device *dev);
-	int (*transmit)(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
+struct net_device_ops {
+    int (*open)(struct net_device *dev);
+    int (*close)(struct net_device *dev);
+    int (*transmit)(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst);
 };
 
 extern struct net_device *
